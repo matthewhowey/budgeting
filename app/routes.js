ee62a191ae5239a8f76b0f8f2c93f630bbecee43
srich-DWP <srich.leeds@gmail.com>
2015-11-27 11:35:14 +0000

    MVPv01 routes update

--- app/routes.js
+++ app/routes.js
@@ -106,41 +106,41 @@ module.exports = {
     //MVPv01
     app.post('/MVPv01/about-benefit', function (req, res) {
         if (req.body.benefit === 'Yes') {
-            res.redirect('/beta02/partner')
+            res.redirect('/MVPv01/partner')
         } else {
-            res.redirect('/beta02/overview')
+            res.redirect('/MVPv01/overview')
         }
     })
 
     app.post('/MVPv01/partner', function (req, res) {
         if (req.body.partner === 'Yes') {
-            res.redirect('/beta02/partner-yes')
+            res.redirect('/MVPv01/partner-yes')
         } else {
-            res.redirect('/beta02/partner-no')
+            res.redirect('/MVPv01/partner-no')
         }
     })
 
     app.post('/MVPv01/money-you-owe', function (req, res) {
         if (req.body.partner === 'Yes') {
-            res.redirect('/beta02/debt-repayment')
+            res.redirect('/MVPv01/debt-repayment')
         } else {
-            res.redirect('/beta02/personal-details')
+            res.redirect('/MVPv01/personal-details')
         }
     })
 
     app.post('/MVPv01/children', function (req, res) {
         if (req.body.children === 'Yes') {
-            res.redirect('/beta02/loan-amount-child')
+            res.redirect('/MVPv01/loan-amount-child')
         } else {
-            res.redirect('/beta02/loan-amount-partner')
+            res.redirect('/MVPv01/loan-amount-partner')
         }
     })
 
     app.post('/MVPv01/children-no-partner', function (req, res) {
         if (req.body.children === 'Yes') {
-            res.redirect('/beta02/loan-amount-child-single')
+            res.redirect('/MVPv01/loan-amount-child-single')
         } else {
-            res.redirect('/beta02/loan-amount-single')
+            res.redirect('/MVPv01/loan-amount-single')
         }
     })
 
